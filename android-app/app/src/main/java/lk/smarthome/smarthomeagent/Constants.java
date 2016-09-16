71352151e7b4ef18648f14f30692ed1e97ec3285
Charitha Goonetilleke <charithag@users.noreply.github.com>
2016-09-16 18:00:56 +0530

    Prepare for tag

--- android-app/app/src/main/java/lk/smarthome/smarthomeagent/Constants.java
+++ android-app/app/src/main/java/lk/smarthome/smarthomeagent/Constants.java
@@ -6,6 +6,6 @@ package lk.smarthome.smarthomeagent;
 public final class Constants {
     public static final String UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
     public static final String ARG_REGION_INDEX = "ARG_REGION_INDEX";
-    public static final String ACTIVEMQ_URL = "tcp://192.168.8.102:1883";
+    public static final String ACTIVEMQ_URL = "tcp://<server ip>:1883";
     public static final String MESSAGE_ARRIVED = "lk.smarthome.smarthomeagent.MESSAGE_ARRIVED";
 }
